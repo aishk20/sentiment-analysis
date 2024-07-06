2130ea33160acfdc488dc55c99cad0de236cd6d4
Aishwarya Karande <apkarande02@gmail.com>
2024-07-06 20:01:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, render_template
+
+
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import nltk
+
+
+
+
+nltk.download('vader_lexicon')
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        inp = request.form.get("inp")
+        sid = SentimentIntensityAnalyzer()
+        score = sid.polarity_scores(inp)
+        if score["neg"] != 0:
+            return render_template("index2.html", message="Negative Feedback ☹️☹️")
+        else:
+            return render_template("index2.html", message="Positive Feed back 😁😁")
+    return render_template("index2.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
